b5f987fea350cfdcd1ea7886c5b33543a4b975dc
winter.loo <adjkldd@126.com>
2023-05-25 02:24:20 +0800

    add viz_gdb_stack.py

---
+++ viz_gdb_stack.py
@@ -0,0 +1,32 @@
+import graphviz
+
+dot = graphviz.Digraph('gdb stack', comment='gdb stack')
+
+relations = dict()
+
+prev_func = None
+curr_func = None
+with open('funcs.txt') as f:
+  for line in f:
+    if line.startswith('---'):
+      prev_func = None
+      continue
+    curr_func = line.strip()
+    if not curr_func in relations:
+      relations[curr_func] = set()
+
+    if prev_func != None:
+      relations[curr_func].add(prev_func)
+
+    prev_func = curr_func
+
+for n in relations.keys():
+  dot.node(n)
+
+for n, nns in relations.items():
+  if len(nns) == 0:
+    continue
+  for nn in nns:
+    dot.edge(n, nn)
+
+print(dot.source)
