7ea0bd076d95c4d6687b8d4ba860db1fcd301c8e
winter.loo <adjkldd@126.com>
2023-05-31 11:43:23 +0800

    reformat

--- pfuncname.py
+++ pfuncname.py
@@ -7,70 +7,82 @@
 #   (gdb) bt
 #   (gdb) set logging enabled off
 #
-import os
 import argparse
 
+
 class Args(object):
-  args = None
+    args = None
+
 
 def filter_gdb_frame(frame):
-  # print('>>>>frame start>>>>')
-  # print(frame.strip())
-  # print('<<<<frame end<<<<')
+    # print('>>>>frame start>>>>')
+    # print(frame.strip())
+    # print('<<<<frame end<<<<')
 
-  idx = frame.find(' (')
-  funcname = frame[:idx].split(' ')[-1]
+    idx = frame.find(' (')
+    funcname = frame[:idx].split(' ')[-1]
 
-  idx = frame.find('at ')
-  if idx != -1:
-    filename, lino = frame[idx + 3:].split(':')
-    if None != Args.args.path_prefix_to_remove:
-      if filename.startswith(Args.args.path_prefix_to_remove):
-        startpos = len(Args.args.path_prefix_to_remove)
-        if not Args.args.path_prefix_to_remove.endswith('/'):
-          startpos += 1
-        filename = filename[startpos:]
+    idx = frame.find('at ')
+    if idx != -1:
+        filename, lino = frame[idx + 3:].split(':')
+        if Args.args.path_prefix_to_remove is not None:
+            if filename.startswith(Args.args.path_prefix_to_remove):
+                startpos = len(Args.args.path_prefix_to_remove)
+                if not Args.args.path_prefix_to_remove.endswith('/'):
+                    startpos += 1
+                filename = filename[startpos:]
 
-  if Args.args.with_loc == True:
-    print(f"{funcname} {filename}:{lino}")
-  else:
-    print(f"{funcname}")
+    if Args.args.with_loc:
+        print(f"{funcname} {filename}:{lino}")
+    else:
+        print(f"{funcname}")
 
 
 def main():
-  with open(Args.args.gdb_logging_file) as f:
-      in_stack = False
-  
-      while True:
-        buf = f.read()
-        if len(buf) == 0:
-          break
-        l = 0
-        r = 0
-  
-        while r < len(buf):
-          if buf[r] == '#' and r + 1 < len(buf) and buf[r + 1] == '0':
-            l = r
-            in_stack = True
-          elif in_stack and r > 0 and buf[r - 1] == '\n' and (buf[r] != '#' and buf[r] != ' '):
-            in_stack = False
-            frame = buf[l:r-1]
-            filter_gdb_frame(frame)
-            print("----STACK FRAME DONE----")
-  
-          if in_stack and (l < r and r > 0 and buf[r - 1] == '\n' and buf[r] == '#'):
-            frame = buf[l:r-1]
-            l = r
-            filter_gdb_frame(frame)
-          r += 1
+    with open(Args.args.gdb_logging_file) as f:
+        in_stack = False
+
+        while True:
+            buf = f.read()
+            if len(buf) == 0:
+                break
+            left = 0
+            r = 0
+
+            while r < len(buf):
+                if buf[r] == '#' and r + 1 < len(buf) and buf[r + 1] == '0':
+                    left = r
+                    in_stack = True
+                elif in_stack and r > 0 and buf[r - 1] == '\n' and (
+                        buf[r] != '#' and buf[r] != ' '):
+                    in_stack = False
+                    frame = buf[left:r - 1]
+                    filter_gdb_frame(frame)
+                    print("----STACK FRAME DONE----")
+
+                if in_stack and (left < r and r > 0 and buf[r - 1] == '\n'
+                                 and buf[r] == '#'):
+                    frame = buf[left:r - 1]
+                    left = r
+                    filter_gdb_frame(frame)
+                r += 1
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='print gdb stack frames')
+    parser = argparse.ArgumentParser(description='print gdb stack frames')
 
-  parser.add_argument('--with-loc', '-l', action='store_true', help='print with location')
-  parser.add_argument('--gdb-logging-file', '-f', default='gdb.txt', help='gdb logging file')
-  parser.add_argument('--path-prefix-to-remove', '-p', help='remove directory prefix from filename')
+    parser.add_argument('--with-loc',
+                        '-l',
+                        action='store_true',
+                        help='print with location')
+    parser.add_argument('--gdb-logging-file',
+                        '-f',
+                        default='gdb.txt',
+                        help='gdb logging file')
+    parser.add_argument('--path-prefix-to-remove',
+                        '-p',
+                        help='remove directory prefix from filename')
 
-  Args.args = parser.parse_args()
+    Args.args = parser.parse_args()
 
-  main()
+    main()
--- viz_gdb_stack.py
+++ viz_gdb_stack.py
@@ -7,26 +7,26 @@ relations = dict()
 prev_func = None
 curr_func = None
 with open('funcs.txt') as f:
-  for line in f:
-    if line.startswith('---'):
-      prev_func = None
-      continue
-    curr_func = line.strip()
-    if not curr_func in relations:
-      relations[curr_func] = set()
+    for line in f:
+        if line.startswith('---'):
+            prev_func = None
+            continue
+        curr_func = line.strip()
+        if curr_func not in relations:
+            relations[curr_func] = set()
 
-    if prev_func != None:
-      relations[curr_func].add(prev_func)
+        if prev_func is not None:
+            relations[curr_func].add(prev_func)
 
-    prev_func = curr_func
+        prev_func = curr_func
 
 for n in relations.keys():
-  dot.node(n)
+    dot.node(n)
 
 for n, nns in relations.items():
-  if len(nns) == 0:
-    continue
-  for nn in nns:
-    dot.edge(n, nn)
+    if len(nns) == 0:
+        continue
+    for nn in nns:
+        dot.edge(n, nn)
 
 print(dot.source)
